7115032f06e036b41d8173c5c8c967430cec71fc
Alice Steele <aksteele@usc.edu>
2023-10-12 21:38:12 +0000

    testing LED pt 2

--- test_suite.py
+++ test_suite.py
@@ -1,4 +1,11 @@
 import RPi.GPIO as GPIO
+import time
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
-GPIO.output(11, GPIO.HIGH)
+
+while(1):
+    GPIO.output(11, GPIO.HIGH)
+    time.sleep(.5)
+    GPIO.output(11, GPIO.LOW)
+    time.sleep(.5)
