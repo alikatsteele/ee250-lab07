47808fea889f770122caa6abcc0f57b3a348f21e
Alice Steele <aksteele@usc.edu>
2023-10-12 22:16:34 +0000

    troubleshooting led blinking

--- test_suite.py
+++ test_suite.py
@@ -20,11 +20,11 @@ while(1):
         GPIO.output(11, GPIO.LOW)
         time.sleep(.5)
         counter = 0
-    while counter < 5:
-        light = mcp.read_adc(0)
-        print(light)
-        if (light < 2):
-            print("dark")
-        else:
-            print("light")
-        time.sleep(0.1)
+    #while counter < 5:
+    #    light = mcp.read_adc(0)
+     #   print(light)
+      #  if (light < 2):
+       #     print("dark")
+        #else:
+         #   print("light")
+        #time.sleep(0.1)
